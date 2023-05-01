67abe59cab88b9555f6d23af035f8be04d8e21a8
David Bachir <83042769+DavidBachir@users.noreply.github.com>
2023-05-01 14:51:07 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,6 +7,7 @@ import pywebio.input as inputt
 import pywebio.output as outputt
 import json
 import os
+import time
 
 
 def input_job_details():
@@ -16,46 +17,57 @@ def input_job_details():
      inputt.textarea( label = "Enter your resume", name='resume', required=True)
      ]
     results = input_group("Enter Info", inputs)
-    print(results["title"])
+
     return results["title"], results["location"], results["resume"]
 
 
-def scrape_jobs(job_title, job_location):
+def scrape_jobs(job_title, job_location, x):
+  
     with outputt.put_loading():
-        put_markdown("## Scraping job listings from Indeed...")
-        jobs = get_jobs_posts_indeed(job_title= job_title, job_location= job_location)
+        
+        put_markdown(f"## Scraping job listings from Indeed... ")
+                
+        jobs = get_jobs_posts_indeed(job_title= job_title, job_location= job_location, count = x)
+        
     if not jobs:
         put_markdown("No job listings found.")
         return None
     else:
         with outputt.put_loading():
             put_markdown("## Saving job listings to JSON file...")
+            time.sleep(2)
             save_jobs_to_json(jobs, 'indeed_jobs')
         return jobs
 
 
-def display_jobs(jobs):
+def display_jobs(jobs, resume = ""):
     with outputt.put_loading():
         put_text("## Reading job listings from JSON file...")
+        
     # Get the current timestamp to construct the filename
         time_stamp = get_time_stamp()
         filename = f'indeed_jobs_{time_stamp}.json'
     # Check if the JSON file exists before attempting to read from it
-    if not os.path.isfile(filename):
-        put_markdown("No job listings found.")
-    else:
-        with open(filename) as f:
-            jobs = json.load(f)
+        if not os.path.isfile(filename):
+            put_markdown("No job listings found.")
+        else:
+            with open(filename) as f:
+                jobs = json.load(f)
+                #print(jobs)
+                time.sleep(1)
+        
 
-        put_markdown("## Job info")
-        for idx, job in enumerate(jobs):
-            outputt.put_link(name  =  f"{job['title']}",  url = f"{job['url']}")
-            #put_markdown(f"### {idx + 1}. {job['title']}")
-            put_text(f"Company: {job['company']}")
-            put_text(f"Location: {job['location']}")
+    put_markdown("## Job info")
+   
+    for idx, job in enumerate(jobs):
+        outputt.put_link(name  =  f"{idx + 1}- {job['title']}",  url = f"{job['url']}")
+        #put_markdown(f"### {idx + 1}. {job['title']}")
+        put_text(f"Company: {job['company']}")
+        put_text(f"Location: {job['location']}")
+        display_compatibility(jobs, resume, idx)
+        
             
-            
-        return jobs
+    return jobs
 
 
 def calculate_compatibility(jobs, resume):
@@ -78,14 +90,14 @@ def save_compatibility_to_file(compatibility_list):
 
 
 
-def display_compatibility(jobs, resume):
+def display_compatibility(jobs, resume, n):
     with outputt.put_loading():
         put_markdown("## Computing compatibility")
         compatibility_list = calculate_compatibility(jobs, resume)
-        
-    put_markdown("## Compatibility Info")
-    outputt.put_scrollable(outputt.put_scope('output1'), height=350, keep_bottom=True)
-    with use_scope('output1'):
+    n =f"output{n}"
+    #put_markdown("## Compatibility Info")
+    outputt.put_collapse("Compatibility Info",outputt.put_scrollable(outputt.put_scope(n), height=350, keep_bottom=True))
+    with use_scope(n):
        
             #put_markdown(f"### {idx + 1}. {jobs[idx]['title']} Compatibility Info")
             put_markdown("#### Compatibility Rating:")
@@ -103,11 +115,12 @@ def display_compatibility(jobs, resume):
 def main():
     put_markdown("# Job Scraper and Compatibility Evaluator")
     job_title, job_location, resume = input_job_details()
-    jobs = scrape_jobs(job_title, job_location)
+    jobs = scrape_jobs(job_title, job_location, 3)
     if jobs:
-        jobs = display_jobs(jobs)
-        
-        display_compatibility(jobs, resume)
+       # for i in range(len(jobs)):
+       jobs = display_jobs(jobs, resume = resume)
+       
+       #display_compatibility(jobs, resume)
 
 
 if __name__ == '__main__':
